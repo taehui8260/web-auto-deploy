a242e2672bb3093e817b945bd207f8600fe01009
taehui8260 <taehui8260@gmail.com>
2024-10-15 12:24:11 +0900

    UPDATE: JAVA 버전 수정
    11 -> 17

---
+++ src/main/java/com/taehui/web/WebApplication.java
@@ -0,0 +1,16 @@
+package com.taehui.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Application
+ */
+@SpringBootApplication
+public class WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApplication.class, args);
+	}
+
+}
