261c9c8ca447c8be923b4a1b99663fbc6e89e2f1
taehui8260 <taehui8260@gmail.com>
2024-10-18 10:05:04 +0900

    Re-add front-end directory

---
+++ src/main/java/com/taehui/web/Controller/LoginController.java
@@ -0,0 +1,27 @@
+package com.taehui.web.Controller;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+
+@Controller
+public class LoginController {
+
+    @PostMapping("/api/login")
+    public ResponseEntity<String>  login(@RequestBody final Map<String, Object> params){
+        String token = "tokenString";
+        return new ResponseEntity<>(token, HttpStatus.OK);
+    }
+    @GetMapping("/api/login")
+    public ResponseEntity<String>  loginGet(@RequestParam final Map<String, Object> params){
+        String token = "tokenString";
+        return new ResponseEntity<>(token, HttpStatus.OK);
+    }
+}
