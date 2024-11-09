0ef8f3c8afa44ccda207ca65324421b8812982d7
taehui8260 <taehui8260@gmail.com>
2024-11-09 09:30:09 +0900

    UPDATE: front-end 설정 변경

---
+++ front-end/pages/login/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <button type="button" @click="login()">
+            login
+        </button>
+        <p>{{token}}</p>
+    </div>
+</template>
+<script>
+export default{
+    data(){
+        return {
+            token: ''
+        }
+    },
+    methods:{
+        async login(){
+            this.token = await this.$axios.$get('/api/login')
+        }
+    }
+}
+</script>
