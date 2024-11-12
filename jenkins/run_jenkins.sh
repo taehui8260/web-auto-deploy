033250da0b98ff57cf1acefd0e21742c4d127952
taehui8260 <taehui8260@gmail.com>
2024-11-12 22:57:06 +0900

    UPDATE: 환경설정
    Jenkins Pipeline, DockerFine

--- jenkins/run_jenkins.sh
+++ jenkins/run_jenkins.sh
@@ -1,7 +1,7 @@
 docker run --rm -d -p 28080:8080 -p 50000:50000 \
         -v /appdata/jenkins/jenkins_home:/var/jenkins_home \
         -v /var/run/docker.sock:/var/run/docker.sock \
-        -v /appdata/deploy:/home/portal/deploy \
+        -v /appdata/web-auto-deploy:/appdata/web-auto-deploy \
         -e TZ=Asia/Seoul \
         -e JENKINS_OPTS="--httpPort=8080" \
         -u root \
