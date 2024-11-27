7a2b26bb040eb78600d3c1bdfcef7f603c173eb7
taehui8260 <taehui8260@gmail.com>
2024-11-27 22:19:29 +0900

    UPDATE: jenkins
    파이프라인 및 실행파일 수정

--- jenkins/run_jenkins.sh
+++ jenkins/run_jenkins.sh
@@ -1,8 +1,8 @@
 docker run --rm -d -p 28080:8080 -p 50000:50000 \
         -v /appdata/jenkins/jenkins_home:/var/jenkins_home \
         -v /var/run/docker.sock:/var/run/docker.sock \
-        -v /appdata/web-auto-deploy:/appdata/web-auto-deploy \
+        -v /appdata/jenkins/web-auto-deploy:/appdata/jenkins/web-auto-deploy \
         -e TZ=Asia/Seoul \
         -e JENKINS_OPTS="--httpPort=8080" \
         -u root \
-        --name jenkins-container jenkins/jenkins
+        --name jenkillns-container jenkins/jenkins
