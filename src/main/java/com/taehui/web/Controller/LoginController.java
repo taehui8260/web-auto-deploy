4d77d19c9f75296e191354a5db1bf980dd253b4f
taehui8260 <taehui8260@gmail.com>
2024-12-01 21:10:48 +0900

    UPDATE: 설정 변경
    Dockerfile 및 pipeline 수정

--- src/main/java/com/taehui/web/Controller/LoginController.java
+++ src/main/java/com/taehui/web/Controller/LoginController.java
@@ -1,6 +1,8 @@
 package com.taehui.web.Controller;
 
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
